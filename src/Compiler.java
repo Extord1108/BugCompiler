4b1c6f20528c3fbc1c14169b6d36fe7f267a79b0
Re-Li-fe <gyf1657261410@outlook.com>
2023-08-07 22:28:13 +0800

    测试用

--- src/Compiler.java
+++ src/Compiler.java
@@ -28,11 +28,11 @@ public class Compiler {
             var visitor = Visitor.Instance;
             visitor.visit(tree);
 
-            if (!arg.llvmFile.isEmpty()) {
-                FileOutputStream llvmOut = OutputHandler.getOutputFile(arg.llvmFile.split("\\.")[0]+"-pure." + arg.llvmFile.split("\\.")[1]);
-                Manager.getManager().outputLLVM(llvmOut);
-                OutputHandler.closeOutputFile(llvmOut);
-            }
+            // if (!arg.llvmFile.isEmpty()) {
+            //     FileOutputStream llvmOut = OutputHandler.getOutputFile(arg.llvmFile.split("\\.")[0]+"-pure." + arg.llvmFile.split("\\.")[1]);
+            //     Manager.getManager().outputLLVM(llvmOut);
+            //     OutputHandler.closeOutputFile(llvmOut);
+            // }
 
             var midEndRunner = new MidEndRunner(Manager.getFunctions(), Manager.getGlobals(), arg.opt);
             midEndRunner.run();
@@ -44,8 +44,8 @@ public class Compiler {
                 OutputHandler.closeOutputFile(llvmOut);
             }
             System.out.println("CodeGen begin");
-            var codeGen = CodeGen.Instance;
-            codeGen.gen();
+            //var codeGen = CodeGen.Instance;
+            //codeGen.gen();
             System.out.println("CodeGen end");
 
             System.out.println("Alloc begin");
