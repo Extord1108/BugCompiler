6fb8c200e023b0bedf9999e3f2afb7a33620908d
Re-Li-fe <gyf1657261410@outlook.com>
2023-08-09 00:18:40 +0800

    gvn debug

--- src/midend/DeadCodeElimination.java
+++ src/midend/DeadCodeElimination.java
@@ -122,7 +122,7 @@ public class DeadCodeElimination extends Pass {
             for(Function function : this.functions.values()) {
                 for(BasicBlock bb : function.getBasicBlocks()){
                     for(Instr instr : bb.getInstrs()){
-                        if(instr.getUsedInfo().size()!=0 || instr instanceof Call || instr instanceof Return || instr instanceof Branch || instr instanceof Jump || instr instanceof Store){
+                        if(instr instanceof Load || instr instanceof Call || instr instanceof Return || instr instanceof Branch || instr instanceof Jump || instr instanceof Store){
                             queue.addLast(instr);
                         }
                     }
