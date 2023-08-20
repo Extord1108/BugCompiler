15b1a36ca0b08ddd5aafde90c8e451c6e076b0ef
mingming <2416013822@qq.com>
2023-08-20 18:36:19 +0800

    nothing

--- src/midend/MidEndRunner.java
+++ src/midend/MidEndRunner.java
@@ -41,7 +41,7 @@ public class MidEndRunner {
             new DomainAnalysis(functions, globals).run();
             new DeadCodeElimination(functions, globals).run();
         }
-//        new PhiResolution(functions, globals).run();
+        new PhiResolution(functions, globals).run();
         return;
     }
 }
